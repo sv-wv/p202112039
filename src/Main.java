cb5cdda396485e8acdcead16aa6dedf492987c33
Kim, Eungyeol <elmm872@gmail.com>
2024-11-20 09:17:52 +0900

    메시지 two

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("202112039 김은결");
+		System.out.println(Hello.sum(10));
+		System.out.println(Hello.sum(100));
 	}
 }
